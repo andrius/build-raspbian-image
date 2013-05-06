cdad183c1e7b5dcd949b6b5f467821192823b20e
Andrius Kairiukstis <andrius@kairiukstis.com>
2013-05-06 11:39:41 +0000

    modified:   ../.gitignore 	modified:   build_raspbian_sd_card.sh

--- raspbian/build_raspbian_sd_card.sh
+++ raspbian/build_raspbian_sd_card.sh
@@ -5,6 +5,10 @@
 # Modifications by
 # Andrius Kairiukstis <andrius@kairiukstis.com>, http://andrius.mobi/
 #
+# 2013-05-05
+#	resulting files will be stored in rpi folder of script
+#	during installation, delivery contents folder will be mounted and install.sh script within it will be called
+#
 # 2013-04-20
 #	distro replaced from Debian Wheezy to Raspbian (http://raspbian.org)
 #	build environment and resulting files not in /tmp/rpi instead of /root/rpi
@@ -56,50 +60,62 @@
 deb_mirror="http://archive.raspbian.org/raspbian"
 deb_local_mirror="http://localhost:3142/archive.raspbian.org/raspbian"
 
-bootsize="64M"
-deb_release="wheezy"
-
-device=$1
-buildenv="/tmp/rpi"
-rootfs="${buildenv}/rootfs"
-bootfs="${rootfs}/boot"
-
-mydate=`date +%Y%m%d`
-
-if [ "$deb_local_mirror" == "" ]; then
-  deb_local_mirror=$deb_mirror
-fi
-
-image=""
-
-
-if [ $EUID -ne 0 ]; then
+if [ ${EUID} -ne 0 ]; then
   echo "this tool must be run as root"
   exit 1
 fi
 
-if ! [ -b $device ]; then
-  echo "$device is not a block device"
+device=$1
+if ! [ -b ${device} ]; then
+  echo "${device} is not a block device"
   exit 1
 fi
 
-if [ "$device" == "" ]; then
-  echo "no block device given, just creating an image"
-  mkdir -p $buildenv
-  image="${buildenv}/raspbian_basic_${deb_release}_${mydate}.img"
-  dd if=/dev/zero of=$image bs=1MB count=2000
-  device=`losetup -f --show $image`
-  echo "image $image created and mounted as $device"
-else
-  dd if=/dev/zero of=$device bs=512 count=1
+if [ "${deb_local_mirror}" == "" ]; then
+  deb_local_mirror=${deb_mirror}
 fi
 
-fdisk $device << EOF
+bootsize="64M"
+deb_release="wheezy"
+
+relative_path=`dirname $0`
+
+# locate path of this script
+absolute_path=`cd ${relative_path}; pwd`
+
+# locate path of delivery content
+delivery_path=`cd ${absolute_path}/../delivery; pwd`
+
+# define destination folder where created image file will be stored
+buildenv=`cd ${absolute_path}; cd ..; mkdir -p rpi; cd rpi; pwd`
+# buildenv="/tmp/rpi"
+
+# cd ${absolute_path}
+
+rootfs="${buildenv}/rootfs"
+bootfs="${rootfs}/boot"
+
+today=`date +%Y%m%d`
+
+image=""
+
+if [ "${device}" == "" ]; then
+  echo "no block device given, just creating an image"
+  mkdir -p ${buildenv}
+  image="${buildenv}/raspbian_basic_${deb_release}_${today}.img"
+  dd if=/dev/zero of=${image} bs=1MB count=2000
+  device=`losetup -f --show ${image}`
+  echo "image ${image} created and mounted as ${device}"
+else
+  dd if=/dev/zero of=${device} bs=512 count=1
+fi
+
+fdisk ${device} << EOF
 n
 p
 1
 
-+$bootsize
++${bootsize}
 t
 c
 n
@@ -111,9 +127,9 @@ w
 EOF
 
 
-if [ "$image" != "" ]; then
-  losetup -d $device
-  device=`kpartx -va $image | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
+if [ "${image}" != "" ]; then
+  losetup -d ${device}
+  device=`kpartx -va ${image} | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
   device="/dev/mapper/${device}"
   bootp=${device}p1
   rootp=${device}p2
@@ -131,33 +147,34 @@ else
   fi
 fi
 
-mkfs.vfat $bootp
-mkfs.ext4 $rootp
+mkfs.vfat ${bootp}
+mkfs.ext4 ${rootp}
 
-mkdir -p $rootfs
+mkdir -p ${rootfs}
 
-mount $rootp $rootfs
+mount ${rootp} ${rootfs}
 
-mkdir -p $rootfs/proc
-mkdir -p $rootfs/sys
-mkdir -p $rootfs/dev
-mkdir -p $rootfs/dev/pts
-mkdir -p $rootfs/usr/src/asterisk
+mkdir -p ${rootfs}/proc
+mkdir -p ${rootfs}/sys
+mkdir -p ${rootfs}/dev
+mkdir -p ${rootfs}/dev/pts
+mkdir -p ${rootfs}/usr/src/delivery
 
-mount -t proc none $rootfs/proc
-mount -t sysfs none $rootfs/sys
-mount -o bind /dev $rootfs/dev
-mount -o bind /dev/pts $rootfs/dev/pts
+mount -t proc none ${rootfs}/proc
+mount -t sysfs none ${rootfs}/sys
+mount -o bind /dev ${rootfs}/dev
+mount -o bind /dev/pts ${rootfs}/dev/pts
+mount -o bind ${delivery_path} ${rootfs}/usr/src/delivery
 
-cd $rootfs
+cd ${rootfs}
 
-debootstrap --foreign --arch armhf $deb_release $rootfs $deb_local_mirror
+debootstrap --foreign --arch armhf ${deb_release} ${rootfs} ${deb_local_mirror}
 cp /usr/bin/qemu-arm-static usr/bin/
-LANG=C chroot $rootfs /debootstrap/debootstrap --second-stage
+LANG=C chroot ${rootfs} /debootstrap/debootstrap --second-stage
 
-mount $bootp $bootfs
+mount ${bootp} ${bootfs}
 
-echo "deb $deb_local_mirror $deb_release main contrib non-free
+echo "deb ${deb_local_mirror} ${deb_release} main contrib non-free
 " > etc/apt/sources.list
 
 echo "dwc_otg.lpm_enable=0 console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait" > boot/cmdline.txt
@@ -179,9 +196,6 @@ echo "vchiq
 snd_bcm2835
 " >> etc/modules
 
-# keymaps listed at /usr/share/keymaps/i386
-# console-common	console-data/keymap/full	select	de-latin1-nodeadkeys
-
 echo "console-common	console-data/keymap/policy	select	Select keymap from full list
 console-common	console-data/keymap/full	select	us
 " > debconf.set
@@ -200,9 +214,9 @@ rpi-update
 
 apt-get -y install locales console-common ntp openssh-server less vim
 
-echo \"execute all necessary commands, then type exit\"
-echo \"asterisk install files located at /usr/src/asterisk\"
-bash
+# execute install script at mounted external media (delivery contents folder)
+cd /usr/src/delivery
+./install.sh
 
 echo \"root:raspberry\" | chpasswd
 sed -i -e 's/KERNEL\!=\"eth\*|/KERNEL\!=\"/' /lib/udev/rules.d/75-persistent-net-generator.rules
@@ -210,9 +224,9 @@ rm -f /etc/udev/rules.d/70-persistent-net.rules
 rm -f third-stage
 " > third-stage
 chmod +x third-stage
-LANG=C chroot $rootfs /third-stage
+LANG=C chroot ${rootfs} /third-stage
 
-echo "deb $deb_mirror $deb_release main contrib non-free
+echo "deb ${deb_mirror} ${deb_release} main contrib non-free
 " > etc/apt/sources.list
 
 echo "#!/bin/bash
@@ -222,28 +236,29 @@ apt-get clean
 rm -f cleanup
 " > cleanup
 chmod +x cleanup
-LANG=C chroot $rootfs /cleanup
+LANG=C chroot ${rootfs} /cleanup
 
 cd
 
 sync
 sleep 15
 
-umount -l $bootp
+umount -l ${bootp}
 
-umount -l $rootfs/dev/pts
-umount -l $rootfs/dev
-umount -l $rootfs/sys
-umount -l $rootfs/proc
+umount -l ${rootfs}/usr/src/delivery
+umount -l ${rootfs}/dev/pts
+umount -l ${rootfs}/dev
+umount -l ${rootfs}/sys
+umount -l ${rootfs}/proc
 
-umount -l $rootfs
-umount -l $rootp
+umount -l ${rootfs}
+umount -l ${rootp}
 
-echo "finishing $image"
+echo "finishing ${image}"
 
-if [ "$image" != "" ]; then
-  kpartx -d $image
-  echo "created image $image"
+if [ "${image}" != "" ]; then
+  kpartx -d ${image}
+  echo "created image ${image}"
 fi
 
 echo "done."
