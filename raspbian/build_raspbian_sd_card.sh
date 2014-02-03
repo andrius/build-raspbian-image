262ebe9e23c3389435b3c45c6bfcc5fe3d3e42b3
Andrius Kairiukstis <andrius@kairiukstis.com>
2014-02-03 21:26:13 +0100

    some bug-fixes, nothing importnt, worked on delivery files
    	modified:   raspbian/build_raspbian_sd_card.sh

--- raspbian/build_raspbian_sd_card.sh
+++ raspbian/build_raspbian_sd_card.sh
@@ -103,7 +103,7 @@ if [ "${device}" == "" ]; then
   echo "no block device given, just creating an image"
   mkdir -p ${buildenv}
   image="${buildenv}/images/raspbian_basic_${deb_release}_${today}.img"
-  dd if=/dev/zero of=${image} bs=1MB count=2000
+  dd if=/dev/zero of=${image} bs=1MB count=3800
   device=`losetup -f --show ${image}`
   echo "image ${image} created and mounted as ${device}"
 else
